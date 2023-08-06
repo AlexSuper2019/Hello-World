567a14044f33410994d7aaae25414fccd83cf634
AlexSuper2019 <56264189+AlexSuper2019@users.noreply.github.com>
2023-08-06 11:31:06 +0300

    third commit, go checking

--- for_test.cpp
+++ for_test.cpp
@@ -5,5 +5,10 @@ using namespace std;
 int main(){
     // Adding some comments for second commit
     cout << "First commit" << endl;
+
+    for (int i = 0; i < 10; ++i){
+        // Я ЛЮБЛЮ С++ AAAAAAAAAA
+        cout << "I LOVE C++ !!!" << endl;
+    }
     
 }
